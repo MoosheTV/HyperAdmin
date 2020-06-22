439332bca3509c7d91df80555e3a8b9d5e247688
Success <rjmxtechnology@gmail.com>
2020-06-22 12:37:53 -0500

    Replace deprecated __resource.lua with fxmanifest.lua

---
+++ hyperadmin/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+game 'gta5'
+
+description 'HyperAdmin by MoosheTV on Github'
+source 'originally released on cfx.re forums and published to github'
+cfxlink 'https://forum.cfx.re/t/release-hyperadmin-standalone-administrative-menu/244274/'
+
+client_script 'HyperAdmin.Client.net.dll'
+server_script 'HyperAdmin.Server.net.dll'
+
+file 'Newtonsoft.Json.dll'
